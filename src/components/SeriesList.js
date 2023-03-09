4b19edd33733afa6383af82d9cf8645a43e1d097
ditod1 <orlandodiazconde@gmail.com>
2023-03-09 11:08:40 -0500

    Update SeriesList.js

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -2,34 +2,36 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   addNewSeries,
+  getSeries,
+  getSeriesData,
   getStatus,
   removeSeries as removeSeriesFromAPI,
 } from '../redux/series/seriesSlice';
 import Form from './Form';
 import Serie from './Serie';
 
-const mockData = [
-  {
-    item_id: '0',
-    title: 'The Last Of Us',
-    director: 'Neil Druckman',
-    category: 'Action',
-    currentEpisode: 20,
-    totalEpisodes: 100,
-  },
-];
+// const mockData = [
+//   {
+//     item_id: '0',
+//     title: 'The Last Of Us',
+//     director: 'Neil Druckman',
+//     category: 'Action',
+//     currentEpisode: 20,
+//     totalEpisodes: 100,
+//   },
+// ];
 
 const mockCategories = ['Action', 'Comedy'];
 
 const SeriesList = () => {
   const status = useSelector(getStatus);
-  // const seriesData = useSelector(getSeriesData);
+  const seriesData = useSelector(getSeriesData);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // if (status === 'idle') {
-    //   dispatch(getSeries());
-    // }
+    if (status === 'idle') {
+      dispatch(getSeries());
+    }
   }, [status, dispatch]);
 
   const addSeries = (series) => {
@@ -40,7 +42,7 @@ const SeriesList = () => {
     dispatch(removeSeriesFromAPI({ data: { item_id: key } }));
   };
 
-  const isLoading = false;
+  const isLoading = status === 'loading';
   return (
     <div className="container-lg py-4">
       <div>
@@ -51,7 +53,7 @@ const SeriesList = () => {
             </div>
           </div>
         ) : (
-          mockData.map((series) => (
+          seriesData.map((series) => (
             <Serie key={series.item_id} series={series} onRemoveSeries={removeSeries} />
           ))
         )}
